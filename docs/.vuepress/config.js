afba6d0996d048debe4e8da62348e061fa97dd26
Roberto Achar <robertoachar@gmail.com>
2018-09-19 09:56:08 -0300

    feature: add vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: 'Workflow Node.js',
+  description: 'Treinamento: Workflow para projetos em Node.js',
+  dest: './build'
+};
---
+++ docs/README.md
@@ -0,0 +1,4 @@
+---
+home: true
+footer: CC-BY-NC-SA-4.0 © 2018 Roberto Achar
+---
