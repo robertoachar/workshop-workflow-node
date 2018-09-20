c3d631acbe40c3a2a8196263ef2c2e18cee5fbf0
Roberto Achar <robertoachar@gmail.com>
2018-09-19 21:46:19 -0300

    build: add service worker

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: 'Workflow Node.js',
   description: 'Treinamento: Workflow para projetos em Node.js',
   dest: './build',
+  serviceWorker: true,
   themeConfig: {
     sidebar: {
       '/workshop/': [
